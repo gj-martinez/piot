08ef150b66d751a7fcfe92f503be32cfa28dd5ba
gj-martinez <gonzamartinezok@gmail.com>
2021-12-28 11:35:37 -0300

    manejo de errores express

--- piot-api/server.js
+++ piot-api/server.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const debug = require('debug')('piot:api')
 const http = require('http')
 const express = require('express')
 
@@ -11,6 +12,25 @@ const server = http.createServer(app)
 
 app.use('/api', api)
 
+// Express Error Handler
+app.use((err, req, res, next) => {
+  debug(`Error: ${err.message}`)
+
+  if (err.message.match(/not found/)) {
+    return res.status(404).send({ error: err.message })
+  }
+
+  res.status(500).send({ error: err.message })
+})
+
+function handleFatalError (err) {
+  console.error(`fatal error ${err.message}`)
+  console.error(err.stack)
+  process.exit(1)
+}
+
+process.on('uncaughtException', handleFatalError)
+process.on('unhandledRejection', handleFatalError)
 server.listen(port, () => {
   console.log(`piot server listening on port ${port}`)
 })
