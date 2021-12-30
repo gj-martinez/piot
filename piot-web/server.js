7f827285f367f08b874b343f360f8fa5339f3866
gj-martinez <gonzamartinezok@gmail.com>
2021-12-30 12:41:12 -0300

    agregar socket a la web

--- piot-web/server.js
+++ piot-web/server.js
@@ -8,8 +8,24 @@ const express = require('express')
 const port = process.env.PORT || 8080
 const app = express()
 const server = http.createServer(app)
+const socketio = require('socket.io')
+const io = socketio(server)
 
 app.use(express.static(path.join(__dirname, 'public')))
+
+
+// Socket.io / WebSockets
+io.on('connect', socket => {
+  debug(`Connected ${socket.id}`)
+
+  socket.on('agent/message', payload => {
+    console.log(payload)
+  })
+
+  setInterval(() => {
+    socket.emit('agent/message', { agent: 'xxx-yyy' })
+  }, 2000)
+})
 
 function handleFatalError (err) {
   console.error(`fatal error:  ${err.message}`)
